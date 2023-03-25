fcf8f39b2184a7e014e84739ee33657b369f075d
Elena <kor-09-06-2008@yandex.ru>
2023-03-25 14:00:42 +0300

    Добавлен Example 01

---
+++ Example01/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne > numberTwo)
+{
+   Console.Write("Большее число: ");
+   Console.WriteLine(numberOne);
+}
+else
+{
+   Console.Write("Большее число: ");
+   Console.WriteLine(numberTwo);
+}
+
+if (numberOne > numberTwo)
+{
+   Console.Write("Меньшее число: ");
+   Console.WriteLine(numberTwo);
+}
+else
+{
+   Console.Write("Меньшее число: ");
+   Console.WriteLine(numberOne);
+}
